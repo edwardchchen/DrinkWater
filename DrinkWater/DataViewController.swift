ee5ec21076acc4ab202d363295e19a8b832c989d
Chih-Yang Edward Chen <c8x2b@ugrad.cs.ubc.ca>
2019-12-19 20:37:25 -0800

    Initial Commit

---
+++ DrinkWater/DataViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DataViewController.swift
+//  DrinkWater
+//
+//  Created by Edward Chen on 2019-12-19.
+//  Copyright © 2019 Edward Chen. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
