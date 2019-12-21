ab674049ab8dddc1e30f18bf501a4baa78fa5a21
Chih-Yang Edward Chen <c8x2b@ugrad.cs.ubc.ca>
2019-12-20 20:45:31 -0800

    add and remove btn works

--- DrinkWater/ViewController.swift
+++ DrinkWater/ViewController.swift
@@ -10,22 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var confirmAddBtn: UIButton!
-    @IBOutlet weak var plusBtn: UIButton!
-    @IBOutlet weak var minusBtn: UIButton!
-    @IBOutlet weak var waterBottleBtn: UIButton!
     @IBOutlet weak var volumeOfWaterAddingLabel: UILabel!
     @IBOutlet var backgroundUIView: UIView!
     
+    @IBAction func addWaterBtn(_ sender: UIButton) {
+        var currentVolumeAdding = Int(volumeOfWaterAddingLabel.text ?? "0")
+        currentVolumeAdding = currentVolumeAdding! + 25
+        volumeOfWaterAddingLabel.text = String(currentVolumeAdding ?? 0)
+    }
     
+    @IBAction func removeWaterBtn(_ sender: UIButton) {
+        var currentVolumeAdding = Int(volumeOfWaterAddingLabel.text ?? "0")
+        currentVolumeAdding = currentVolumeAdding! - 25
+        volumeOfWaterAddingLabel.text = String(currentVolumeAdding ?? 0)
+    }
+    @IBAction func addIntoBottleBtn(_ sender: UIButton) {
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        volumeOfWaterAddingLabel.text = "0"
         // Do any additional setup after loading the view.
     }
     
-    func addWater(plusBtn : UIButton) {
-    }
 
 
 }
