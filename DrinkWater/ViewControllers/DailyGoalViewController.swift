6eca0b63c0c9d7f82fa6fd3002ed8bb5670d04db
Chih-Yang Edward Chen <c8x2b@ugrad.cs.ubc.ca>
2019-12-29 17:07:36 -0800

    used pickerview in daily goal, added save functionality

--- DrinkWater/ViewControllers/DailyGoalViewController.swift
+++ DrinkWater/ViewControllers/DailyGoalViewController.swift
@@ -10,6 +10,19 @@ import Foundation
 import UIKit
 
 class DailyGoalViewController : UIViewController {
+    
+    var dailyGoal = 2000
+    let volumeInMetric = [1600,1800,2000,2200,2400,2600,
+    2800,3000,3200,3400, 3600, 3800, 4000]
+    
+    let defaults = UserDefaults.standard
+
+    struct Keys {
+        static let goal = "dailyGoal"
+        static let unitpref = "unitPreference"
+    }
+    
+    
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var unitLabel: UILabel!
     
@@ -17,12 +30,33 @@ class DailyGoalViewController : UIViewController {
         saveDailyGoal()
     }
     
-    var dailyGoal = 2000
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadDailyGoal()
         textField.text = String(dailyGoal)
         createTap()
+        createPicker()
+    }
+
+    
+    func saveDailyGoal() {
+        dailyGoal = Int(textField.text!)!
+        defaults.set(dailyGoal, forKey: Keys.goal)
+        print("save %i", dailyGoal)
+    }
+    
+    func loadDailyGoal() {
+        dailyGoal = defaults.integer(forKey: Keys.goal)
+        print(dailyGoal)
+        textField.text = String(dailyGoal)
+    }
+    
+    func createPicker() {
+        let picker = UIPickerView()
+        picker.delegate = self
+        picker.dataSource = self
+        textField.inputView = picker
     }
 
     
@@ -31,13 +65,26 @@ class DailyGoalViewController : UIViewController {
         tap.cancelsTouchesInView = false
         view.addGestureRecognizer(tap)
     }
-    
-    func saveDailyGoal() {
-        if (textField.text != nil) {
-            dailyGoal = Int(textField.text ?? "2000")!
-        }
-    }
-    
 
     
+}
+extension DailyGoalViewController : UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return volumeInMetric.count
+    }
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return String(volumeInMetric[row])
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        dailyGoal = volumeInMetric[row]
+        textField.text = String(dailyGoal)
+    }
 }
