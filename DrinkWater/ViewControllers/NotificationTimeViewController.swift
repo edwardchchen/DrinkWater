2dd0f64f434f4923df67a2c180f66956fcdf22cd
Chih-Yang Edward Chen <c8x2b@ugrad.cs.ubc.ca>
2019-12-30 17:13:07 -0800

    notification time UI done

--- DrinkWater/ViewControllers/NotificationTimeViewController.swift
+++ DrinkWater/ViewControllers/NotificationTimeViewController.swift
@@ -9,6 +9,14 @@
 import Foundation
 import UIKit
 
-class NotificationTimeViewController {
+class NotificationTimeViewController :UIViewController {
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var startTimeTextField: UITextField!
     
+    @IBAction func setButtonPressed(_ sender: UIButton) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
 }
